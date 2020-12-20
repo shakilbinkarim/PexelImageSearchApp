d50cb48add1b82d1a59b7cf7177795a29bf32e2d
Shakil Bin Karim <shakilbinkarim@outlook.com>
2020-12-21 05:23:29 +0900

    action bar

--- app/src/main/java/io/github/shakilbinkarim/pexelimagesearchapp/view/MainActivity.kt
+++ app/src/main/java/io/github/shakilbinkarim/pexelimagesearchapp/view/MainActivity.kt
@@ -2,11 +2,33 @@ package io.github.shakilbinkarim.pexelimagesearchapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import io.github.shakilbinkarim.pexelimagesearchapp.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBar()
     }
+
+    private fun setupActionBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_nav_host_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        val appBarConfig = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
